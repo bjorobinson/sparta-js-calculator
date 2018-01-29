8c15d4b986337e211d32fc1d0f840685e341e73d
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-01-29 22:23:48 +0000

    Added check on every setting to ensure user at least input the right number of terms

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ switch (mode) {
     console.log("Basic");
     var action = prompt("Input a basic sum with two integers, seperated by space, and using +, -, *, or /");
     var calc = action.split(" ");
+    if (calc.length !=3){
+      throw 'InvalidInputError';
+    }
     var operator = calc[1];
     calc[0] = parseInt(calc[0]);
     calc[2] = parseInt(calc[2]);
@@ -28,6 +31,9 @@ switch (mode) {
   case 'a':
     var action = prompt("Input square or square root function in the for: 2^3 or | 8");
     var calc = action.split(" ");
+    if (calc.length !=3 || calc.length != 2){
+      throw 'InvalidInputError';
+    }
     if (calc[1]==='^'){
       calc[0] = parseInt(calc[0]);
       calc[2] = parseInt(calc[2]);
@@ -43,6 +49,9 @@ switch (mode) {
   case 'bmi':
     var action = prompt("Enter your height(meters or inches), weight (kilograms and pounds), and number system used (met)ric or (imp)erial");
     var calc = action.split(" ");
+    if (calc.length !=3 ){
+      throw 'InvalidInputError';
+    }
     calc[0] = parseInt(calc[0]);
     calc[1] = parseInt(calc[1]);
     if (calc[2]==='imp') {
@@ -57,6 +66,9 @@ switch (mode) {
   case 't':
     var action = prompt("Enter distance (miles), mpg, cost per gallon(in pounds, but don't include symbol) and speed (mph) all seperated by a space and recieve an evaluated time and cost")
     var calc = action.split(" ");
+    if (calc.length != 4){
+      throw 'InvalidInputError'
+    }
     var mpg_reduction = 0;
     if (calc[3] > 60){
       mpg_reduction = 2 * (calc[3]-60)
