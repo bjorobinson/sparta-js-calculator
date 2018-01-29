cace7fa933e3d67b760a7385b1cee41385df07d4
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-01-29 20:35:35 +0000

    Uses split to get sum input in a predictable format

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-mode = prompt("Input mode: (b)asic, (a)dvanced")
+var mode = prompt("Input mode: (b)asic, (a)dvanced")
 switch (mode) {
   case 'b':
     console.log("Basic");
+    var action = prompt("Input a basic sum with two integers, seperated by space, and using +, -, *, or /");
+    var calc = action.split(" ");
+    var operator = calc[1];
+    switch (operator) {
+      case '+':
+        console.log('+');
+        break;
+      case '-':
+        console.log('-');
+        break;
+      case '*':
+        console.log('*');
+        break;
+      case '/':
+        console.log('/');
+        break;
+      default:
+        console.log("invalid sum");
+        throw 'Error';
+    }
+    console.log(calc);
     break;
   case 'a':
     console.log("Advanced");
