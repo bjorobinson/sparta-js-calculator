d230c81b937840efa4d0bdc90b855e7bdc0d2803
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-01-29 21:00:17 +0000

    Added BMI calculator with metric and imperial systems

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var mode = prompt("Input mode: (b)asic, (a)dvanced")
+var mode = prompt("Input mode: (b)asic, (a)dvanced, (bmi) calculator");
 switch (mode) {
   case 'b':
     console.log("Basic");
@@ -26,19 +26,33 @@ switch (mode) {
     console.log(calc);
     break;
   case 'a':
-  var action = prompt("Input square or square root function in the for: 2^3 or | 8");
-  var calc = action.split(" ");
-  if (calc[1]==='^'){
-    calc[0] = parseInt(calc[0]);
-    calc[2] = parseInt(calc[2]);
-    alert(Math.pow(calc[0],calc[2]));
-  } else if (calc[0]==='|') {
-    calc[1] = parseInt(calc[1])
-    alert(Math.sqrt(calc[1]))
-  } else {
-    throw 'InvalidInputError';
-  }
+    var action = prompt("Input square or square root function in the for: 2^3 or | 8");
+    var calc = action.split(" ");
+    if (calc[1]==='^'){
+      calc[0] = parseInt(calc[0]);
+      calc[2] = parseInt(calc[2]);
+      alert(Math.pow(calc[0],calc[2]));
+    } else if (calc[0]==='|') {
+      calc[1] = parseInt(calc[1])
+      alert(Math.sqrt(calc[1]))
+    } else {
+      throw 'InvalidInputError';
+    }
     break;
+
+  case 'bmi':
+    var action = prompt("Enter your height(meters or inches), weight (kilograms and pounds), and number system used (met)ric or (imp)erial");
+    var calc = action.split(" ");
+    calc[0] = parseInt(calc[0]);
+    calc[1] = parseInt(calc[1]);
+    if (calc[2]==='imp') {
+      alert(calc[1]/(Math.pow(calc[0],2))*703);
+    } else if (calc[2]==='met') {
+      alert(calc[1]/(Math.pow(calc[0],2)));
+    } else {
+      throw 'InvalidInputError'
+    }
+
   default:
     console.log("Quitting");
 }
